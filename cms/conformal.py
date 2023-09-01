a45f5786592521344a550bbcff378413beec670c
Mario beraha <berahamario@gmail.com>
2023-09-01 08:42:48 +0200

    resuing models and streams

--- cms/conformal.py
+++ cms/conformal.py
@@ -312,6 +312,7 @@ class ConformalCMS:
         self.unique = unique
         self.two_sided = two_sided
         self.agg_rule = agg_rule
+        self.model = None
 
     @lru_cache(maxsize=2048)
     def _predict_interval(self, x, scorer=None, t_hat_low=None, t_hat_upp=None):
@@ -327,32 +328,32 @@ class ConformalCMS:
             upper = self.cms.estimate_count(x)
 
         return lower + lower_warmup, upper + lower_warmup
+    
+    def change_rule(self, new_rule):
+        if self.model is None:
+            raise RuntimeError("chane_rule can be called only after run")
+        
+        self.model.rule = new_rule
 
-    def run(self, n, n_test, confidence=0.9, seed=2021, heavy_hitters_gamma=0.01, shift=0):
-        n_bins = self.n_bins
-        scorer_type = self.scorer_type
-
-        print("Running conformal method with n = {:d}...".format(n))
-        sys.stdout.flush()
-
+    def warmup(self):
         ## Warmup
         print("Warm-up iterations (max track: {:d})...".format(self.max_track))
         sys.stdout.flush()
-        freq_track = defaultdict(lambda: 0)
-        data_track = defaultdict(lambda: 0)
+        self.freq_track = defaultdict(lambda: 0)
+        self.data_track = defaultdict(lambda: 0)
         self.cms_warmup = copy.deepcopy(self.cms)
-        #n_track = 0
-        n_track = self.max_track
         i_range=tqdm(range(self.max_track), disable=False)
-        train_data = []
+        self.train_data = []
         for i in i_range:
             x = self.stream.sample()
             self.cms_warmup.update_count(x)
-            freq_track[x] = 0
-            data_track[x] += 1
-            train_data.append(x)
+            self.freq_track[x] = 0
+            self.data_track[x] += 1
+            self.train_data.append(x)
 
-        n1 = n - n_track
+    def consume_stream(self, niter):
+        n1 = niter - self.max_track
+        
         print("Main iterations: {:d}...".format(n1))
         sys.stdout.flush()
         # Process stream
@@ -361,146 +362,109 @@ class ConformalCMS:
             self.cms.update_count(x)
 
             # Check whether this object is being tracked
-            if x in freq_track.keys():
-                freq_track[x] += 1
+            if x in self.freq_track.keys():
+                self.freq_track[x] += 1
 
-        if n1 > 0:
-            print("Calibrating lower bound....")
-            sys.stdout.flush()
-
-            J = self.cms.w
-
-            if scorer_type == "Bayesian-DP":
-                model = BayesianDP(self.cms, agg_rule=self.agg_rule)
-                _ = model.empirical_bayes()
-                if self.two_sided:
-                    scorer = BayesianScoresTwoSided(model, 1.0-confidence)
-                else:
-                    scorer = BayesianScores(model, 1.0-confidence)
-
-            elif scorer_type == "Bayesian-NGG":
-                model = SmoothedNGG(self.cms, train_data, agg_rule=self.agg_rule)
-                _ = model.empirical_bayes()
-                if self.two_sided:
-                    scorer = BayesianScoresTwoSided(model, 1.0-confidence)
-                else:
-                    scorer = BayesianScores(model, 1.0-confidence)
-
-            elif scorer_type == "Bootstrap":
-                if self.two_sided:
-                    scorer = BootstrapScoresTwoSided(self.cms, 1.0-confidence)
-                else:
-                    scorer = BootstrapScores(self.cms, 1.0-confidence)
-
-            elif scorer_type == "Constant":
-                if self.two_sided:
-                    scorer = ClassicalScoresTwoSided(self.cms, method="constant")
-                else:
-                    scorer = ClassicalScores(self.cms, method="constant")
-
-            elif scorer_type == "Proportional":
-                scorer = ClassicalScores(self.cms, method="proportional")
-
-            elif scorer_type == "Adaptive":
-                scorer = QRScores(self.cms, confidence, seed=seed, two_sided=self.two_sided)
-                #scorer = HRScores(self.cms, confidence, seed=seed)
-
-                # Split the tracking data into training/calibration sets
-                if self.unique > 1:
-                    Y_track = np.array(list(freq_track.values()))
-                    object_track = np.array(list(freq_track.keys()))
-                    upper_track_list = np.array([self.cms.estimate_count(x) for x in freq_track.keys()])
-                    freq_track_keys = np.array(list(freq_track.keys()))
-                else:
-                    Y_track = np.concatenate([[freq_track[x]]*data_track[x] for x in freq_track.keys()])
-                    object_track = np.concatenate([[x]*data_track[x] for x in freq_track.keys()])
-                    upper_track_list = np.concatenate([[self.cms.estimate_count(x)]*data_track[x] for x in freq_track.keys()])
-                    freq_track_keys = np.concatenate([[x]*data_track[x] for x in freq_track.keys()])
-
-                upper_track = upper_track_list.reshape((len(upper_track_list),1))
-                upper_train, upper_calib, Y_train, Y_calib, _, freq_track_keys_calib = train_test_split(upper_track, Y_track, freq_track_keys,
-                                                                                                        test_size=1-self.prop_train, random_state=2022)
-                # Train the predictive model
-                scorer.train(upper_train, Y_train, X_calib=upper_calib, Y_calib=Y_calib)
-                # Repackage the calibration data
-                freq_track = defaultdict(lambda: 0)
-                data_track = defaultdict(lambda: 0)
-                for i in range(len(freq_track_keys_calib)):
-                    x = freq_track_keys_calib[i]
-                    freq_track[x] = Y_calib[i]
-                    data_track[x] = np.sum(freq_track_keys_calib==x)
-
+    def create_and_fit_model(self, confidence):
+        n_bins = self.n_bins
+        scorer_type = self.scorer_type
+        if scorer_type == "Bayesian-DP":
+            self.model = BayesianDP(self.cms, agg_rule=self.agg_rule)
+            _ = self.model.empirical_bayes()
+            if self.two_sided:
+                self.scorer = BayesianScoresTwoSided(self.model, 1.0-confidence)
             else:
-                raise("Error! Unknown type of scores.")
+                self.scorer = BayesianScores(self.model, 1.0-confidence)
 
-            if self.unique > 1:
-                scores_cal_tmp = np.concatenate([[scorer.compute_score(x, freq_track[x])]*data_track[x] for x in tqdm(freq_track.keys(), disable=False)])
-                y_cal_tmp = np.concatenate([[freq_track[x]]*data_track[x] for x in freq_track.keys()])
-                x_cal_tmp = np.concatenate([[x]*data_track[x] for x in freq_track.keys()])
-                # Split calibration data points into subsets
-                G = int(np.maximum(2,np.floor(len(y_cal_tmp)/self.unique)))
-                scores_cal = np.zeros((G,2))
-                y_cal = np.zeros((G,))
-                kf = KFold(n_splits=G, random_state=2022, shuffle=True)
-                idx_fold_list = [idx for _, idx in kf.split(x_cal_tmp)]
-                for g in range(G):
-                    idx_fold = idx_fold_list[g]
-                    _, idx_unique = np.unique(x_cal_tmp[idx_fold], return_index=True)
-                    idx_pick = np.random.choice(idx_unique)
-                    scores_cal[g] = scores_cal_tmp[idx_fold][idx_pick]
-                    y_cal[g] = y_cal_tmp[idx_fold][idx_pick]
-
+        elif scorer_type == "Bayesian-NGG":
+            self.model = SmoothedNGG(self.cms, self.train_data, agg_rule=self.agg_rule)
+            _ = self.model.empirical_bayes()
+            if self.two_sided:
+                self.scorer = BayesianScoresTwoSided(self.model, 1.0-confidence)
             else:
-                # Calculate scores
-                scores_cal = np.concatenate([[scorer.compute_score(x, freq_track[x])]*data_track[x] for x in tqdm(freq_track.keys(), disable=False)])
-                y_cal = np.concatenate([[freq_track[x]]*data_track[x] for x in freq_track.keys()])
+                self.scorer = BayesianScores(self.model, 1.0-confidence)
 
-            # Calibrate the conformity scores (for bin-conditional coverage)
-            n_bins_max = int(np.maximum(1, np.floor(len(y_cal)/100)))
-            n_bins = np.minimum(n_bins, n_bins_max)
-            y_bins, y_bin_cutoffs = pd.qcut(y_cal, n_bins, duplicates="drop", labels=False, retbins=True, precision=0)
-            print("Cutoffs for {:d} bins:".format(n_bins))
-            print(y_bin_cutoffs)
+    def run(self, n, n_test, confidence=0.9, seed=2021, heavy_hitters_gamma=0.01, shift=0, 
+            reuse_stream=False, reuse_model=False):
+        n_bins = self.n_bins
+        scorer_type = self.scorer_type
 
-            calibrated_scores_bins_low = [None]*n_bins
-            calibrated_scores_bins_upp = [None]*n_bins
-            for k in range(n_bins):
-                idx_bin = np.where(y_bins==k)[0]
-                n_bin = len(idx_bin)
-                alpha = 1.0 - confidence
-                if len(idx_bin) > 0:
-                    if self.two_sided:
-                        level_adjusted = (1.0-alpha/2)*(1.0+1.0/float(n_bin))
-                    else:
-                        level_adjusted = (1.0-alpha)*(1.0+1.0/float(n_bin))
+        print("Running conformal method with n = {:d}...".format(n))
+        sys.stdout.flush()
 
-                    calibrated_scores_bins_low[k] = mquantiles(scores_cal[idx_bin,0], prob=level_adjusted)[0]
-                    calibrated_scores_bins_upp[k] = mquantiles(scores_cal[idx_bin,1], prob=level_adjusted)[0]
-                    if not self.two_sided:
-                        calibrated_scores_bins_low[k] = np.ceil(calibrated_scores_bins_low[k]).astype(int)
-                        calibrated_scores_bins_upp[k] = np.ceil(calibrated_scores_bins_upp[k]).astype(int)
-                else:
-                    calibrated_scores_bins_low[k] = 0
-                    calibrated_scores_bins_upp[k] = 0
-            calibrated_score_low = np.max(calibrated_scores_bins_low)
-            calibrated_score_upp = np.max(calibrated_scores_bins_upp)
-            print("Calibrated scores (low):")
-            print(calibrated_scores_bins_low)
-            print("Calibrated scores (upp):")
-            print(calibrated_scores_bins_upp)
-            print("Calibrated score (final):")
-            print([calibrated_score_low, calibrated_score_upp])
+        n1 = n - self.max_track
+        if not reuse_stream:
+            self.warmup()
+            self.consume_stream(n)
 
-            # Combine warm-up and regular cms
-            self.cms.count = self.cms.count + self.cms_warmup.count
-            self.cms.true_count = sum_dict(self.cms.true_count, self.cms_warmup.true_count)
+        if not reuse_model:
+            self.create_and_fit_model(confidence)
+        
+        freq_track = self.freq_track
+        data_track = self.data_track
+        scorer = self.scorer
+        
+        if self.unique > 1:
+            scores_cal_tmp = np.concatenate([[scorer.compute_score(x, freq_track[x])]*data_track[x] for x in tqdm(freq_track.keys(), disable=False)])
+            y_cal_tmp = np.concatenate([[freq_track[x]]*data_track[x] for x in freq_track.keys()])
+            x_cal_tmp = np.concatenate([[x]*data_track[x] for x in freq_track.keys()])
+            # Split calibration data points into subsets
+            G = int(np.maximum(2,np.floor(len(y_cal_tmp)/self.unique)))
+            scores_cal = np.zeros((G,2))
+            y_cal = np.zeros((G,))
+            kf = KFold(n_splits=G, random_state=2022, shuffle=True)
+            idx_fold_list = [idx for _, idx in kf.split(x_cal_tmp)]
+            for g in range(G):
+                idx_fold = idx_fold_list[g]
+                _, idx_unique = np.unique(x_cal_tmp[idx_fold], return_index=True)
+                idx_pick = np.random.choice(idx_unique)
+                scores_cal[g] = scores_cal_tmp[idx_fold][idx_pick]
+                y_cal[g] = y_cal_tmp[idx_fold][idx_pick]
+
         else:
-            scorer = None
-            calibrated_score_low = None
-            calibrated_score_upp = None
-            # Combine warm-up and regular cms
-            self.cms.count = self.cms_warmup.count
-            self.cms.true_count = self.cms_warmup.true_count
+            # Calculate scores
+            scores_cal = np.concatenate([[scorer.compute_score(x, freq_track[x])]*data_track[x] for x in tqdm(freq_track.keys(), disable=False)])
+            y_cal = np.concatenate([[freq_track[x]]*data_track[x] for x in freq_track.keys()])
+
+        # Calibrate the conformity scores (for bin-conditional coverage)
+        n_bins_max = int(np.maximum(1, np.floor(len(y_cal)/100)))
+        n_bins = np.minimum(n_bins, n_bins_max)
+        y_bins, y_bin_cutoffs = pd.qcut(y_cal, n_bins, duplicates="drop", labels=False, retbins=True, precision=0)
+        print("Cutoffs for {:d} bins:".format(n_bins))
+        print(y_bin_cutoffs)
+
+        calibrated_scores_bins_low = [None]*n_bins
+        calibrated_scores_bins_upp = [None]*n_bins
+        for k in range(n_bins):
+            idx_bin = np.where(y_bins==k)[0]
+            n_bin = len(idx_bin)
+            alpha = 1.0 - confidence
+            if len(idx_bin) > 0:
+                if self.two_sided:
+                    level_adjusted = (1.0-alpha/2)*(1.0+1.0/float(n_bin))
+                else:
+                    level_adjusted = (1.0-alpha)*(1.0+1.0/float(n_bin))
+
+                calibrated_scores_bins_low[k] = mquantiles(scores_cal[idx_bin,0], prob=level_adjusted)[0]
+                calibrated_scores_bins_upp[k] = mquantiles(scores_cal[idx_bin,1], prob=level_adjusted)[0]
+                if not self.two_sided:
+                    calibrated_scores_bins_low[k] = np.ceil(calibrated_scores_bins_low[k]).astype(int)
+                    calibrated_scores_bins_upp[k] = np.ceil(calibrated_scores_bins_upp[k]).astype(int)
+            else:
+                calibrated_scores_bins_low[k] = 0
+                calibrated_scores_bins_upp[k] = 0
+        calibrated_score_low = np.max(calibrated_scores_bins_low)
+        calibrated_score_upp = np.max(calibrated_scores_bins_upp)
+        print("Calibrated scores (low):")
+        print(calibrated_scores_bins_low)
+        print("Calibrated scores (upp):")
+        print(calibrated_scores_bins_upp)
+        print("Calibrated score (final):")
+        print([calibrated_score_low, calibrated_score_upp])
+
+        # Combine warm-up and regular cms
+        self.cms.count = self.cms.count + self.cms_warmup.count
+        self.cms.true_count = sum_dict(self.cms.true_count, self.cms_warmup.true_count)
 
 
         # Evaluate
